2021f104ff43c398a644e40d48f70011590662b0
Przemek Kawecki Toshiba <przemek.kawecki@gmail.com>
2021-09-14 14:47:42 +0200

    completed first stage

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,38 @@
-let hamburger = document.querySelector('.logo-image');
-console.log('hb', hamburger);
+// settings
+const select = {
+    hamburger: "img[class='hamburger-image']",
+    foldable_menu: '.foldable-menu',
+
+};
+
+const classNames = {
+    nav: {
+        active: 'active',
+    },
+
+};
+
+
+const app = {
+    initNav: function() {
+        const thisApp = this;
+
+        thisApp.hamburgerIcon = document.querySelector(select.hamburger);
+        thisApp.foldable_menu = document.querySelector(select.foldable_menu);
+
+        console.log(thisApp.hamburgerIcon);
+        console.log(thisApp.foldable_menu);
+
+        thisApp.hamburgerIcon.addEventListener('click', function() {
+            thisApp.foldable_menu.classList.toggle(classNames.nav.active);
+        });
+    },
+
+    init: function() {
+        const thisApp = this;
+
+        thisApp.initNav();
+    },
+};
+
+app.init();
