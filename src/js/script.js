c5bc547307afdbbffec9719ad29e7f9556ea6cc6
Przemek Kawecki <przemek.kawecki@gmail.com>
2022-04-14 13:16:34 +0200

    node-sass version upgraded do comply with node ^16.0.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,38 @@
 // settings
 const select = {
-    hamburger: "img[class='hamburger-image']",
-    foldable_menu: '.foldable-menu',
+  hamburger: 'img[class=\'hamburger-image\']',
+  foldable_menu: '.foldable-menu',
 
 };
 
 const classNames = {
-    nav: {
-        active: 'active',
-    },
+  nav: {
+    active: 'active',
+  },
 
 };
 
 
 const app = {
-    initNav: function() {
-        const thisApp = this;
+  initNav: function() {
+    const thisApp = this;
 
-        thisApp.hamburgerIcon = document.querySelector(select.hamburger);
-        thisApp.foldable_menu = document.querySelector(select.foldable_menu);
+    thisApp.hamburgerIcon = document.querySelector(select.hamburger);
+    thisApp.foldable_menu = document.querySelector(select.foldable_menu);
 
-        console.log(thisApp.hamburgerIcon);
-        console.log(thisApp.foldable_menu);
+    console.log(thisApp.hamburgerIcon);
+    console.log(thisApp.foldable_menu);
 
-        thisApp.hamburgerIcon.addEventListener('click', function() {
-            thisApp.foldable_menu.classList.toggle(classNames.nav.active);
-        });
-    },
+    thisApp.hamburgerIcon.addEventListener('click', function() {
+      thisApp.foldable_menu.classList.toggle(classNames.nav.active);
+    });
+  },
 
-    init: function() {
-        const thisApp = this;
+  init: function() {
+    const thisApp = this;
 
-        thisApp.initNav();
-    },
+    thisApp.initNav();
+  },
 };
 
 app.init();
